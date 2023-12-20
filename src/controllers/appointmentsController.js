9c5bf1f396022bfb4cf41be7d924ef851889e16a
Pablo de Marcos <pademarcos@gmail.com>
2023-12-20 14:15:48 -0300

    delete appointment

--- src/controllers/appointmentsController.js
+++ src/controllers/appointmentsController.js
@@ -2,6 +2,7 @@ const Appointment = require('../models/appointment');
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
 const appointmentsValidators = require('../validators/appointmentsValidators');
+const { verifyAdminPermissions } = require('../controllers/usersController');
 
 const appointmentsController = {};
 
@@ -25,6 +26,46 @@ appointmentsController.addAppointment = async (req, res, next) => {
     await newAppointment.save();
 
     res.status(201).json({ message: 'Turno registrado exitosamente' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+appointmentsController.updateAppointment = async (req, res, next) => {
+  try {
+    verifyAdminPermissions(req, res, next);
+    
+    const { appointmentId, newDoctor, newDate } = req.body;
+
+    if (newDate) {
+      appointmentsValidators.validateAddAppointment({ date: newDate });
+    }
+
+    if (!newDoctor && !newDate) {
+      return res.status(400).json({ message: 'Se requiere al menos un nuevo médico o una nueva fecha' });
+    }
+
+    const appointment = await Appointment.findById(appointmentId);
+    if (!appointment) {
+      return res.status(404).json({ message: 'Cita no encontrada' });
+    }
+
+    // Verificar si el turno está reservado
+    if (appointment.isReserved) {
+      return res.status(400).json({ message: 'No se puede actualizar un turno reservado' });
+    }    
+
+    if (newDoctor) {
+      appointment.doctor = newDoctor;
+    }
+
+    if (newDate) {
+      appointment.date = newDate;
+    }
+
+    await appointment.save();
+
+    res.json({ message: 'Cita actualizada exitosamente' });
   } catch (error) {
     next(error);
   }
@@ -88,6 +129,30 @@ appointmentsController.cancelAppointment = async (req, res, next) => {
     } else {
       return res.status(400).json({ message: 'La cita no está reservada' });
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+appointmentsController.deleteAppointment = async (req, res, next) => {
+  try {
+    const { appointmentId } = req.params;
+
+    // Buscar el turno en la base de datos
+    const appointment = await Appointment.findById(appointmentId);
+    if (!appointment) {
+      return res.status(404).json({ message: 'Turno no encontrado' });
+    }
+
+    // Verificar si el turno está reservado
+    if (appointment.isReserved) {
+      return res.status(400).json({ message: 'No se puede eliminar un turno reservado' });
+    }
+
+    // Eliminar el turno de la base de datos
+    await Appointment.findByIdAndDelete(appointmentId);
+
+    res.json({ message: 'Turno eliminado exitosamente' });
   } catch (error) {
     next(error);
   }
--- src/routes/appointments.js
+++ src/routes/appointments.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const appointmentsController = require('../controllers/appointmentsController');
 const appointmentsValidators = require('../validators/appointmentsValidators');
+const { verifyAdminPermissions } = require('../controllers/usersController');
 
 router.post('/add', appointmentsValidators.validateAddAppointment(), appointmentsController.addAppointment);
 router.put('/reserve/', appointmentsController.reserveAppointment);
 router.put('/cancel/', appointmentsController.cancelAppointment);
+router.put('/update/', verifyAdminPermissions, appointmentsController.updateAppointment);
+router.delete('/delete/:appointmentId', verifyAdminPermissions, appointmentsController.deleteAppointment);
 
 
 module.exports = router;
